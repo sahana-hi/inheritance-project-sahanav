de8d8a07508179f031b1ea6c85837a2eb31f217f
sahana-hi <sahana.venkatr@gmail.com>
2025-04-19 03:33:11 +0000

    finished college student class

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -2,7 +2,29 @@ public class CollegeStudent extends Student{
     private String major;
     private int year;
 
-    public CollegeStudent(String name, int age, String gender, int ID, double gradAvg, int year, String major) {
-        super();
+    public CollegeStudent(String name, int age, String gender, int idNum, double gpa, int year, String major) {
+        super(name, age, gender, idNum, gpa);
+        this.major = major;
+        this.year = year;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMajor(String m) {
+        major = m;
+    }
+
+    public void setYear(int y) {
+        year = y;
+    }
+
+    public String toString() {
+        return super.toString() + "year : " + year + ", major : " + major;
     }
 }
