16ad8951658b0002d5a8a2c3cd686dbafa0e94cd
sahana-hi <sahana.venkatr@gmail.com>
2025-04-19 03:27:49 +0000

    :)

---
+++ CollegeStudent.java
@@ -0,0 +1,8 @@
+public class CollegeStudent extends Student{
+    private String major;
+    private int year;
+
+    public CollegeStudent(String name, int age, String gender, int ID, double gradAvg, int year, String major) {
+        super();
+    }
+}
